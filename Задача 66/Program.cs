6fc1d5b783d291fe6131c01dc9cac796ee3f57ba
Tamara-spr <tamara.90@mail.ru>
2022-10-15 22:57:54 +0300

    ДЗ 9

---
+++ Задача 66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumDigits(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + SumDigits(m, n - 1);
+}
+
+int m = ReadNumber("Введите первое число:");
+int n = ReadNumber("Введите второе число:");
+Console.WriteLine($"Сумма элементов = {SumDigits(m, n)}");
---
+++ Задача 68/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Function(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Function(m - 1, 1);
+    else
+        return Function(m - 1, Function(m, n - 1));
+}
+int m = ReadNumber("Введите неотрицательное число M: ");
+int n = ReadNumber("Введите неотрицательное число N: ");
+Console.WriteLine(Function(m, n));
